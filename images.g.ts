80de1ff0f379d78a4150ec7b9b5c44f7c40788f0
MichTD <rvvw4rskjk@privaterelay.appleid.com>
2026-01-13 19:58:03 -0600

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts, images.g.jres, images.g.ts

--- images.g.ts
+++ images.g.ts
@@ -9,17 +9,17 @@ namespace myImages {
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . f f f f f . . . . . . 
-. . . f f f f f f f f . . . . . 
-. . . f f 1 1 1 f 1 1 1 . . . . 
-. f f f f 1 1 f 1 1 1 f 1 . . . 
-. f f f f 1 1 1 f 1 1 1 f . . . 
-. f f f f f f f f f f f f . . . 
-. f f f f f f f f f f f f . . . 
-. f f f f f f f f f f f f . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
+. . . . . . 1 1 f . 1 1 f . . . 
+. . . . . f 1 1 1 f 1 1 1 . . . 
+. . . . . f 1 1 1 f 1 1 1 . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . . f f f f f f f f f f . . . 
+. . f f f f f f f f f . . . . . 
+. . f f f f f f f f f . . . . . 
+. . f f f f f f f f . . . . . . 
+. . f f f f f f f f . . . . . . 
+. . f f f f f f f f . . . . . . 
 . . . . . . . . . . . . . . . . 
 `;
         }
